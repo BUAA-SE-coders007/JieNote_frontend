baea8cd1ef6f920f2a5510c276be71fb1c4466a3
dongfengwuyishi <149307614+dongfengwuyishi@users.noreply.github.com>
2025-04-12 23:16:16 +0800

    文献管理页面新功能开发

---
+++ src/layouts/Admin.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <sidebar />
+    <div class="relative md:ml-64 bg-blueGray-100">
+      <router-view />
+    </div>
+  </div>
+</template>
+<script>
+import Sidebar from "@/components/Sidebar/Sidebar.vue";
+
+export default {
+  name: "admin-layout",
+  components: {
+    Sidebar,
+  },
+};
+</script>
