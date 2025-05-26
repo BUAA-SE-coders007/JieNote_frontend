83c45f64f54734269f98d0d68a7655acaf920196
田培瑄 <14688869+tian-peixuan@user.noreply.gitee.com>
2025-05-26 16:17:58 +0800

    [feat]:文献库初版（暂无个性推荐）

---
+++ src/api/database.js
@@ -0,0 +1,50 @@
+import http from '@/utils/http'
+
+/**
+ * 文献相关 API
+ */
+
+/**
+ * 搜索文献
+ * @param {Object} params - 查询参数
+ * @param {string} params.query - 搜索关键词（必需）
+ * @returns {Promise<any>} 搜索结果
+ */
+export const searchLiterature = (params = {}) => {
+  return http.get('/database/search', { params });
+};
+
+/**
+ * 获取文献列表
+ * @param {Object} params - 查询参数（可选）
+ * @returns {Promise<any>} 文献列表
+ */
+export const getLiteratureList = (params = {}) => {
+  return http.get('/database/get', { params });
+};
+
+/**
+ * 获取用户自己的文件夹列表
+ * @returns {Promise<any>} 文件夹列表
+ */
+export const getSelfFolders = () => {
+  return http.get('/article/getSelfFolders');
+};
+
+/**
+ * 文献转存到文件夹
+ * @param {Object} params
+ * @param {number|string} params.folder_id - 文件夹ID
+ * @param {number|string} params.article_id - 文献ID
+ * @returns {Promise<any>} 操作结果
+ */
+export const copyArticleToFolder = (params = {}) => {
+  return http.put('/database/copy', null, { params });
+};
+
+export default {
+  searchLiterature,
+  getLiteratureList,
+  getSelfFolders,
+  copyArticleToFolder
+};
